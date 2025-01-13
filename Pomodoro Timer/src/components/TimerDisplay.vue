86cf403ae0153cb8dc4068da117b6f36da94a3d5
Hércules Spencer <hercules.spencer@luukal.com>
2025-01-13 14:02:06 -0100

    PTA-1: Setting basic environment and TimerDisplay component

---
+++ Pomodoro Timer/src/components/TimerDisplay.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="timer-display">
+    <h1>{{ minutes }}:{{ seconds }}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      minutes: 0,
+      seconds: 0,
+      intervalId: 0,
+    }
+  },
+  mounted() {
+    this.startTimer()
+  },
+  beforeunmount() {
+    this.stopTimer()
+  },
+  methods: {
+    startTimer() {
+      this.intervalId = setInterval(() => {
+        this.seconds++
+        if (this.seconds === 60) {
+          this.minutes++
+          this.seconds = 0
+        }
+      }, 1000)
+    },
+    stopTimer() {
+      clearInterval(this.intervalId)
+    },
+  },
+}
+</script>
+
+<style scoped>
+.timer-display {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-size: 2rem;
+  font-weight: bold;
+  color: aqua;
+}
+</style>
---
+++ Pomodoro Timer/src/views/TimerView.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import TimerDisplay from '../components/TimerDisplay.vue'
+</script>
+
+<template>
+  <div class="timer">
+    <h1>This is the timer page</h1>
+    <TimerDisplay />
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
