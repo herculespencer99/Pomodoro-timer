09e5d84a2031685c2b33900b9e6a7e0e4be06168
Hércules Spencer <hercules.spencer@luukal.com>
2025-01-14 22:24:41 -0100

    PTA: wip

--- Pomodoro Timer/src/components/TimerDisplay.vue
+++ Pomodoro Timer/src/components/TimerDisplay.vue
@@ -1,53 +1,96 @@
 <template>
   <div class="timer-display">
     <h1>{{ String(minutes).padStart(2, '0') }}:{{ String(seconds).padStart(2, '0') }}</h1>
-    <ControlButtons :isRunning="isRunning" @pause="stopTimer" @reset="startTimer" />
+    <div v-if="currentTab === 'pomodoro'" class="pomodoro-info">
+      <h2>Pomodoro Timer</h2>
+      <p>Repetitions: {{ repetitions }}</p>
+      <p>Interval: {{ interval }} minutes</p>
+    </div>
+    <div v-else-if="currentTab === 'shortBreak'" class="short-break-info">
+      <h2>Short Break</h2>
+      <p>Duration: {{ shortBreakDuration }} minutes</p>
+    </div>
+    <div v-else-if="currentTab === 'longBreak'" class="long-break-info">
+      <h2>Long Break</h2>
+      <p>Duration: {{ longBreakDuration }} minutes</p>
+    </div>
+    <div class="control-buttons">
+      <button @click="startTimer">Start</button>
+      <button @click="pauseTimer">Pause</button>
+      <button @click="resetTimer">Reset</button>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import ControlButtons from './ControlButtons.vue'
 export default {
   data() {
     return {
       minutes: 0,
       seconds: 0,
       intervalId: 0,
-      isRunning: true,
+      isRunning: false,
+      repetitions: 4,
+      interval: 25,
+      shortBreakDuration: 5,
+      longBreakDuration: 15,
+      currentTab: 'pomodoro',
+      currentRepetition: 1,
     }
-  },
-  mounted() {
-    this.startTimer()
-  },
-  beforeunmount() {
-    this.stopTimer()
   },
   methods: {
     startTimer() {
+      this.isRunning = true
       this.intervalId = setInterval(() => {
         if (this.isRunning) {
-          this.seconds++
-          if (this.seconds === 60) {
-            this.minutes++
-            this.seconds = 0
+          if (this.seconds > 0) {
+            this.seconds--
+          } else if (this.minutes > 0) {
+            this.minutes--
+            this.seconds = 59
+          } else {
+            this.handleTimerComplete()
           }
         }
       }, 1000)
     },
-    stopTimer() {
-      clearInterval(this.intervalId)
-    },
     pauseTimer() {
-      this.isRunning = !this.isRunning
+      this.isRunning = false
+      clearInterval(this.intervalId)
     },
     resetTimer() {
       this.isRunning = false
-      this.minutes = 0
+      clearInterval(this.intervalId)
+      this.minutes = this.interval
       this.seconds = 0
+      this.currentTab = 'pomodoro'
+      this.currentRepetition = 1
     },
-  },
-  components: {
-    ControlButtons,
+    handleTimerComplete() {
+      if (this.currentTab === 'pomodoro') {
+        this.playSound('alarm.mp3')
+        this.currentTab = 'shortBreak'
+        this.minutes = this.shortBreakDuration
+        this.seconds = 0
+      } else if (this.currentTab === 'shortBreak' || this.currentTab === 'longBreak') {
+        this.currentRepetition++
+        if (this.currentRepetition <= this.repetitions) {
+          this.currentTab = 'pomodoro'
+          this.minutes = this.interval
+          this.seconds = 0
+        } else {
+          this.currentTab = 'longBreak'
+          this.minutes = this.longBreakDuration
+          this.seconds = 0
+        }
+      } else if (this.currentTab === 'longBreak') {
+        this.resetTimer()
+      }
+    },
+    playSound(file: string) {
+      const audio = new Audio(file)
+      audio.play()
+    },
   },
 }
 </script>
@@ -67,5 +110,49 @@ export default {
   font-weight: bold;
   color: aqua;
   background-color: #262626;
+}
+
+button {
+  padding: 20px 50px;
+  min-width: 3rem;
+  border-radius: 5px;
+  border: 4px solid aqua;
+  font-size: 20px;
+  background-color: #181818;
+  color: aqua;
+  cursor: pointer;
+}
+
+.pomodoro-info,
+.short-break-info,
+.long-break-info {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+}
+
+.pomodoro-info h2,
+.short-break-info h2,
+.long-break-info h2 {
+  font-size: 2.5rem;
+  font-weight: 600;
+}
+
+.pomodoro-info p,
+.short-break-info p,
+.long-break-info p {
+  font-size: 1.5rem;
+  font-weight: 400;
+}
+
+.control-buttons {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  height: 100px;
+  gap: 20px;
 }
 </style>
